3bf2bc346777b046ff5eca0085d2f8d7267ce86d
Kikuzawa <Kotelevets-K-0905@yandex.ru>
2024-04-01 09:33:49 +0300

    Создал проект, сделал окно входа, подключил к DB с помощью MySQL

---
+++ JavaLanguage/JavaLangProgProject/src/main/java/com/kiku/javalangprogproject/MainMenuController.java
@@ -0,0 +1,31 @@
+package com.kiku.javalangprogproject;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainMenuController {
+    @FXML
+    public Button buttonReturn;
+
+    private Stage stage;
+    private Scene scene;
+
+
+
+    @FXML
+    public void switchToLoginPage(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("loginPage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
