6030daa28c23f6d5ca31fb6e67867e45c30da0d5
Kikuzawa <Kotelevets-K-0905@yandex.ru>
2024-09-18 22:09:57 +0300

    сделал первую лабу по теории информации (язык C# в WindowsForms) с графическим интерфейсом

---
+++ InformationTheory/laboratory1/laboratory1/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace laboratory1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.runButton = new System.Windows.Forms.Button();
+            this.uploadButton = new System.Windows.Forms.Button();
+            this.inputNameFileBox = new System.Windows.Forms.TextBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.HresultLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(13, 21);
+            this.chart.Name = "chart";
+            this.chart.Size = new System.Drawing.Size(791, 544);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "chart1";
+            title1.Name = "Title1";
+            title1.Text = "Character Frequencies";
+            this.chart.Titles.Add(title1);
+            this.chart.Click += new System.EventHandler(this.chart_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.runButton);
+            this.groupBox1.Controls.Add(this.uploadButton);
+            this.groupBox1.Controls.Add(this.inputNameFileBox);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.inputBox);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(312, 300);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ввод";
+            // 
+            // runButton
+            // 
+            this.runButton.Location = new System.Drawing.Point(117, 262);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(75, 23);
+            this.runButton.TabIndex = 5;
+            this.runButton.Text = "Запустить";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // uploadButton
+            // 
+            this.uploadButton.Enabled = false;
+            this.uploadButton.Location = new System.Drawing.Point(222, 166);
+            this.uploadButton.Name = "uploadButton";
+            this.uploadButton.Size = new System.Drawing.Size(75, 23);
+            this.uploadButton.TabIndex = 4;
+            this.uploadButton.Text = "Выбрать";
+            this.uploadButton.UseVisualStyleBackColor = true;
+            this.uploadButton.Click += new System.EventHandler(this.uploadButton_Click);
+            // 
+            // inputNameFileBox
+            // 
+            this.inputNameFileBox.Enabled = false;
+            this.inputNameFileBox.Location = new System.Drawing.Point(15, 195);
+            this.inputNameFileBox.Name = "inputNameFileBox";
+            this.inputNameFileBox.ReadOnly = true;
+            this.inputNameFileBox.Size = new System.Drawing.Size(282, 20);
+            this.inputNameFileBox.TabIndex = 3;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(15, 162);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(74, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Text = "Файл (txt)";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(15, 54);
+            this.inputBox.Multiline = true;
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(282, 101);
+            this.inputBox.TabIndex = 1;
+            this.inputBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(15, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(55, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Текст";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.HresultLabel);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.chart);
+            this.groupBox2.Location = new System.Drawing.Point(348, 29);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(824, 668);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Вывод";
+            // 
+            // HresultLabel
+            // 
+            this.HresultLabel.AutoSize = true;
+            this.HresultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HresultLabel.Location = new System.Drawing.Point(73, 604);
+            this.HresultLabel.Name = "HresultLabel";
+            this.HresultLabel.Size = new System.Drawing.Size(63, 25);
+            this.HresultLabel.TabIndex = 2;
+            this.HresultLabel.Text = "None";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 604);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "H = ";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AllowUserToOrderColumns = true;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 336);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.Size = new System.Drawing.Size(311, 361);
+            this.dataGridView.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 761);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.Button uploadButton;
+        private System.Windows.Forms.TextBox inputNameFileBox;
+        private System.Windows.Forms.Label HresultLabel;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.DataGridView dataGridView;
+    }
+}
+
---
+++ InformationTheory/laboratory1/laboratory1/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace laboratory1
+{
+    public partial class Form1 : Form
+    {
+        private string inputText;
+        private HashSet<char> allowedChars;
+        
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            allowedChars = new HashSet<char>(
+               " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+-*/().,".ToCharArray() +
+               " абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789+-*/().,"
+           );
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uploadButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.AddExtension = true;
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "TXT files (*.txt)|*.txt";
+
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                foreach (string fileName in openFileDialog.FileNames)
+                {
+                    inputNameFileBox.Text = fileName;
+                }
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            inputBox.Enabled = false;
+            inputNameFileBox.Enabled = true;
+            uploadButton.Enabled = true;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            inputNameFileBox.Enabled = false;
+            uploadButton.Enabled = false;
+            inputBox.Enabled = true;
+
+        }
+
+        private void runButton_Click(object sender, EventArgs e)
+
+        {
+            
+
+
+            if (radioButton1.Checked)
+            {
+                inputText = inputBox.Text;
+            }
+            else if (radioButton2.Checked)
+            {
+                if (!string.IsNullOrEmpty(inputNameFileBox.Text))
+                {
+                    using (StreamReader reader = new StreamReader(inputNameFileBox.Text))
+                    {
+                        inputText = reader.ReadToEnd();
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Please select a file or enter a filename.", "Error");
+                    return;
+                }
+            }
+
+
+
+
+            if (!string.IsNullOrEmpty(inputText))
+            {
+                var charFrequencies = inputText.ToLower().Where(c => allowedChars.Contains(c))
+                    .GroupBy(c => c)
+                    .OrderBy(g => g.Key)
+                    .Select(g => new { Character = g.Key, Frequency = g.Count() })
+                    .ToList();
+
+                // Расчет энтропии
+                double entropy = CalculateEntropy(charFrequencies.Select(f => (f.Character, f.Frequency)).ToList());
+
+                // Отображение результатов
+                dataGridView.DataSource = charFrequencies;
+                dataGridView.Columns["Character"].HeaderText = "Character";
+                dataGridView.Columns["Frequency"].HeaderText = "Frequency";
+
+                // Отображение энтропии
+                HresultLabel.Text = entropy.ToString();
+
+                CreateHistogram();
+
+            }
+            else
+            {
+                MessageBox.Show("No text entered or selected.", "Error");
+            }
+
+
+
+
+        }
+
+        private double CalculateEntropy(List<(char Character, int Frequency)> frequencies)
+        {
+            double entropy = 0;
+            
+            foreach (var (character, frequency) in frequencies)
+            {
+                double probability = (double)frequency / inputText.Length;
+                entropy -= probability * Math.Log(probability, 2);
+            }
+            return entropy;
+        }
+
+
+        private void CreateHistogram()
+        {
+            
+
+            chart.Series.Clear();
+
+
+          
+ 
+                Series series = new Series("Character Frequencies");
+           
+
+            // Очищаем существующую серию от всех точек данных
+            series.Points.Clear();
+
+            foreach (DataGridViewRow row in dataGridView.Rows)
+            {
+                string character = row.Cells["Character"].Value?.ToString() ?? "";
+                int frequency = Convert.ToInt32(row.Cells["Frequency"].Value);
+
+                series.Points.AddXY(character, frequency);
+            }
+
+            chart.Series.Add(series);
+
+            // Настройка осей и шкафа
+            chart.ChartAreas[0].AxisX.LabelStyle.Angle = -45; // Горизонтальные метки
+            chart.ChartAreas[0].AxisX.Interval = 1; // Интервал между метками
+            chart.Legends[0].Enabled = true; // Включение легенды
+
+            
+            
+        }
+
+        private void chart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        
+    }
+
+}
+
