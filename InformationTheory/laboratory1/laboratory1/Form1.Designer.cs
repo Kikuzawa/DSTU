1011fe6e5defcaf4e45d5b9e9cf52205d916c2f3
kikuzawa-cyber <kikuzawa-cyber@yandex.ru>
2024-10-09 13:08:08 +0300

    допилил вторую половину первой лабы, т.е. чтение любого файла

--- InformationTheory/laboratory1/laboratory1/Form1.Designer.cs
+++ InformationTheory/laboratory1/laboratory1/Form1.Designer.cs
@@ -40,11 +40,12 @@
             this.inputBox = new System.Windows.Forms.TextBox();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.IResultLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.HresultLabel = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView = new System.Windows.Forms.DataGridView();
-            this.label2 = new System.Windows.Forms.Label();
-            this.IResultLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -59,7 +60,7 @@
             this.chart.Legends.Add(legend2);
             this.chart.Location = new System.Drawing.Point(13, 21);
             this.chart.Name = "chart";
-            this.chart.Size = new System.Drawing.Size(791, 544);
+            this.chart.Size = new System.Drawing.Size(1187, 544);
             this.chart.TabIndex = 0;
             this.chart.Text = "chart1";
             title2.Name = "Title1";
@@ -117,9 +118,9 @@
             this.radioButton2.AutoSize = true;
             this.radioButton2.Location = new System.Drawing.Point(15, 162);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(74, 17);
+            this.radioButton2.Size = new System.Drawing.Size(54, 17);
             this.radioButton2.TabIndex = 2;
-            this.radioButton2.Text = "Файл (txt)";
+            this.radioButton2.Text = "Файл";
             this.radioButton2.UseVisualStyleBackColor = true;
             this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
@@ -154,10 +155,30 @@
             this.groupBox2.Controls.Add(this.chart);
             this.groupBox2.Location = new System.Drawing.Point(348, 29);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(824, 668);
+            this.groupBox2.Size = new System.Drawing.Size(1233, 668);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Вывод";
+            // 
+            // IResultLabel
+            // 
+            this.IResultLabel.AutoSize = true;
+            this.IResultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IResultLabel.Location = new System.Drawing.Point(73, 629);
+            this.IResultLabel.Name = "IResultLabel";
+            this.IResultLabel.Size = new System.Drawing.Size(63, 25);
+            this.IResultLabel.TabIndex = 4;
+            this.IResultLabel.Text = "None";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 629);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "I = ";
             // 
             // HresultLabel
             // 
@@ -191,31 +212,19 @@
             this.dataGridView.Size = new System.Drawing.Size(311, 361);
             this.dataGridView.TabIndex = 3;
             // 
-            // label2
+            // textBox1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(26, 629);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 25);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "I = ";
-            // 
-            // IResultLabel
-            // 
-            this.IResultLabel.AutoSize = true;
-            this.IResultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.IResultLabel.Location = new System.Drawing.Point(73, 629);
-            this.IResultLabel.Name = "IResultLabel";
-            this.IResultLabel.Size = new System.Drawing.Size(63, 25);
-            this.IResultLabel.TabIndex = 4;
-            this.IResultLabel.Text = "None";
+            this.textBox1.Location = new System.Drawing.Point(27, 720);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1366, 20);
+            this.textBox1.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1184, 761);
+            this.ClientSize = new System.Drawing.Size(1593, 761);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridView);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -230,6 +239,7 @@
             this.groupBox2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -249,6 +259,7 @@
         public System.Windows.Forms.DataGridView dataGridView;
         private System.Windows.Forms.Label IResultLabel;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- InformationTheory/laboratory1/laboratory1/Form1.cs
+++ InformationTheory/laboratory1/laboratory1/Form1.cs
@@ -12,8 +12,8 @@ namespace laboratory1
     {
         private string inputText;
         private HashSet<char> allowedChars;
-        
-        
+
+
 
         public Form1()
         {
@@ -35,7 +35,7 @@ namespace laboratory1
             openFileDialog.CheckFileExists = true;
             openFileDialog.AddExtension = true;
             openFileDialog.Multiselect = true;
-            openFileDialog.Filter = "TXT files (*.txt)|*.txt";
+
 
             if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
@@ -64,12 +64,27 @@ namespace laboratory1
         private void runButton_Click(object sender, EventArgs e)
 
         {
-            
-
-
             if (radioButton1.Checked)
             {
                 inputText = inputBox.Text;
+
+                var charFrequencies = inputText.ToLower().Where(c => allowedChars.Contains(c))
+                            .GroupBy(c => c)
+                            .OrderBy(g => g.Key)
+                            .Select(g => new { Character = g.Key, Frequency = g.Count() })
+                            .ToList();
+
+                double entropy = CalculateEntropy(charFrequencies.Select(f => (f.Character, f.Frequency)).ToList());
+                double kol_info = entropy * inputText.Length / 8 / 1024;
+
+                dataGridView.DataSource = charFrequencies;
+                dataGridView.Columns["Character"].HeaderText = "Character";
+                dataGridView.Columns["Frequency"].HeaderText = "Frequency";
+
+                HresultLabel.Text = entropy.ToString();
+                IResultLabel.Text = kol_info.ToString() + " KB or " + (kol_info / 1024).ToString() + " MB";
+
+                CreateHistogramForText();
             }
             else if (radioButton2.Checked)
             {
@@ -78,57 +93,97 @@ namespace laboratory1
                     using (StreamReader reader = new StreamReader(inputNameFileBox.Text))
                     {
                         inputText = reader.ReadToEnd();
-                    }
 
+                    }
+                    Boolean flag = IsTextFile(inputNameFileBox.Text);
+
+                    if (flag)
+                    {
+                        var charFrequencies = inputText.ToLower().Where(c => allowedChars.Contains(c))
+                            .GroupBy(c => c)
+                            .OrderBy(g => g.Key)
+                            .Select(g => new { Character = g.Key, Frequency = g.Count() })
+                            .ToList();
+
+                        double entropy = CalculateEntropy(charFrequencies.Select(f => (f.Character, f.Frequency)).ToList());
+                        double kol_info = entropy * inputText.Length / 8 / 1024;
+
+                        dataGridView.DataSource = charFrequencies;
+                        dataGridView.Columns["Character"].HeaderText = "Character";
+                        dataGridView.Columns["Frequency"].HeaderText = "Frequency";
+
+                        HresultLabel.Text = entropy.ToString();
+                        IResultLabel.Text = kol_info.ToString() + " KB or " + (kol_info / 1024).ToString() + " MB";
+
+                        CreateHistogramForText();
+                    }
+                    else
+                    {
+
+                        byte[] fileBytes = GetByteArrayFromFile(inputNameFileBox.Text);
+
+                        var byteFrequencies = fileBytes
+                        .GroupBy(b => b)
+                        .OrderBy(g => g.Key)
+                        .Select(g => new { ByteValue = g.Key, Frequency = g.Count() })
+                        .ToList();
+
+                        double entropy = CalculateEntropy(byteFrequencies.Select(f => (f.ByteValue, f.Frequency)).ToList());
+                        double kol_info = entropy * inputText.Length / 8 / 1024;
+
+                        dataGridView.DataSource = byteFrequencies;
+                        dataGridView.Columns["ByteValue"].HeaderText = "Character";
+                        dataGridView.Columns["Frequency"].HeaderText = "Frequency";
+
+                        HresultLabel.Text = entropy.ToString();
+                        IResultLabel.Text = kol_info.ToString() + " KB or " + (kol_info / 1024).ToString() + " MB";
+
+                        CreateHistogramForFile();
+                    }
                 }
                 else
                 {
-                    MessageBox.Show("Please select a file or enter a filename.", "Error");
+                    textBox1.Text = "Please select a file.";
                     return;
                 }
             }
 
-
-
-
-            if (!string.IsNullOrEmpty(inputText))
-            {
-                var charFrequencies = inputText.ToLower().Where(c => allowedChars.Contains(c))
-                    .GroupBy(c => c)
-                    .OrderBy(g => g.Key)
-                    .Select(g => new { Character = g.Key, Frequency = g.Count() })
-                    .ToList();
-
-                // Расчет энтропии
-                double entropy = CalculateEntropy(charFrequencies.Select(f => (f.Character, f.Frequency)).ToList());
-                double kol_info = entropy * inputText.Length / 8 / 1024;
-
-                // Отображение результатов
-                dataGridView.DataSource = charFrequencies;
-                dataGridView.Columns["Character"].HeaderText = "Character";
-                dataGridView.Columns["Frequency"].HeaderText = "Frequency";
-
-                // Отображение энтропии
-                HresultLabel.Text = entropy.ToString();
-                IResultLabel.Text = kol_info.ToString() + " KB or " + (kol_info / 1024).ToString() + " MB";
-
-                CreateHistogram();
-
-            }
-            else
-            {
-                MessageBox.Show("No text entered or selected.", "Error");
-            }
-
-
-
-
+            
         }
+
+
+        public static byte[] GetByteArrayFromFile(string filePath)
+        {
+            byte[] fileBytes;
+
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                fileBytes = new byte[fileStream.Length];
+                fileStream.Read(fileBytes, 0, fileBytes.Length);
+            }
+
+            return fileBytes;
+        }
+
+
+        private bool IsTextFile(string filePath)
+        {
+            string fileExtension = Path.GetExtension(filePath);
+            string[] textFileExtensions = {
+        ".txt", ".csv", ".xml", ".json", ".log", ".md", ".markdown", ".rst", ".ini", ".cfg", ".conf",
+        ".bat", ".cmd", ".sh", ".bash", ".zsh", ".ps1", ".sql", ".css", ".less", ".sass", ".scss",
+        ".html", ".htm", ".xhtml", ".php", ".asp", ".aspx", ".jsp", ".jspx", ".jsf", ".jspx"
+    }; // add more extensions as needed
+
+            return textFileExtensions.Contains(fileExtension, StringComparer.OrdinalIgnoreCase);
+        }
+
+
 
         private double CalculateEntropy(List<(char Character, int Frequency)> frequencies)
         {
             double entropy = 0;
-            
+
             foreach (var (character, frequency) in frequencies)
             {
                 double probability = (double)frequency / inputText.Length;
@@ -137,18 +192,53 @@ namespace laboratory1
             return entropy;
         }
 
-
-        private void CreateHistogram()
+        private double CalculateEntropy(List<(byte ByteValue, int Frequency)> frequencies)
         {
-            
+            double entropy = 0;
+            int totalBytes = frequencies.Sum(f => f.Frequency);
 
+            foreach (var (byteValue, frequency) in frequencies)
+            {
+                double probability = (double)frequency / totalBytes;
+                entropy -= probability * Math.Log(probability, 2);
+            }
+            return entropy;
+        }
+
+        private void CreateHistogramForFile()
+        {
             chart.Series.Clear();
 
+            Series series = new Series("Character Frequencies");
 
-          
- 
-                Series series = new Series("Character Frequencies");
-           
+
+            // Очищаем существующую серию от всех точек данных
+            series.Points.Clear();
+
+            foreach (DataGridViewRow row in dataGridView.Rows)
+            {
+                string character = row.Cells["ByteValue"].Value?.ToString() ?? "";
+                int frequency = Convert.ToInt32(row.Cells["Frequency"].Value);
+
+                series.Points.AddXY(character, frequency);
+            }
+
+            chart.Series.Add(series);
+
+            // Настройка осей и шкафа
+            chart.ChartAreas[0].AxisX.LabelStyle.Angle = -90; // Горизонтальные метки
+            chart.ChartAreas[0].AxisX.Interval = 1; // Интервал между метками (every 6 elements)
+            chart.ChartAreas[0].AxisX.LabelStyle.Interval = 6; // Show labels every 6 elements
+            chart.Legends[0].Enabled = false; // Включение легенды
+        }
+
+
+        private void CreateHistogramForText()
+        {
+            chart.Series.Clear();
+
+            Series series = new Series("Character Frequencies");
+
 
             // Очищаем существующую серию от всех точек данных
             series.Points.Clear();
@@ -167,10 +257,8 @@ namespace laboratory1
             chart.ChartAreas[0].AxisX.LabelStyle.Angle = -45; // Горизонтальные метки
             chart.ChartAreas[0].AxisX.Interval = 1; // Интервал между метками
             chart.Legends[0].Enabled = true; // Включение легенды
-
-            
-            
         }
+
 
         private void chart_Click(object sender, EventArgs e)
         {
